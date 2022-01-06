f9f2cc78986661a6ab175b804232a383c8ae0111
Yashvant Gupta <mlp.yashvantgupta@gmail.com>
2022-01-06 22:20:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+
+  function loginclose() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  function signup() {
+    document.getElementById("signupForm").style.display = "block";
+  }
+  
+  function signupclose() {
+    document.getElementById("signupForm").style.display = "none";
+  }
+
+
+  
