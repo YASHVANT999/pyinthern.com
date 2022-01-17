9b36286070de950a1f45b588937c023461c338ff
Yashvant Gupta <mlp.yashvantgupta@gmail.com>
2022-01-17 20:35:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ function cap() {
 
   var final = a + b + c + d + e + f;
   document.getElementById("capt").value = final;
+  document.getElementById("signup_capt").value = final;
 }
 function validcap() {
   var stg1 = document.getElementById("capt").value;
