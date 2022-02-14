f890d4da0de1da24bea302b70901e9ed23d29182
Yashvant Gupta <mlp.yashvantgupta@gmail.com>
2022-02-14 12:28:46 +0530

    Add files via upload

---
+++ REGISTRATION FORM/app.js
@@ -0,0 +1,114 @@
+//for captcha purpose
+function cap() {
+  var alpha = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "+",
+  ];
+  var a = alpha[Math.floor(Math.random() * 71)];
+  var b = alpha[Math.floor(Math.random() * 71)];
+  var c = alpha[Math.floor(Math.random() * 71)];
+  var d = alpha[Math.floor(Math.random() * 71)];
+  var e = alpha[Math.floor(Math.random() * 71)];
+  var f = alpha[Math.floor(Math.random() * 71)];
+
+  var final = a + b + c + d + e + f;
+  document.getElementById("capt").value = final;
+  document.getElementById("signup_capt").value = final;
+}
+function validcap() {
+  var stg1 = document.getElementById("capt").value;
+  var stg2 = document.getElementById("textinput").value;
+  if (stg1 == stg2) {
+    alert("Form validated successfully");
+    return true;
+  } else {
+    alert("Please enter a valid captcha");
+    return false;
+  }
+}
+
+cap();
+
+// api code
+var form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  })
+    .then((response) => response.json())
+    .then((html) => {
+      // you can put any JS code here
+      alert("success");
+    });
+});
--- app.js
+++ app.js
@@ -192,3 +192,19 @@ function remove_2() {
 function logout() {
   window.location.reload();
 }
+
+//CONTACT US API
+var form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  })
+    .then((response) => response.json())
+    .then((html) => {
+      // you can put any JS code here
+      alert("success");
+    });
+  window.location.reload();
+});
